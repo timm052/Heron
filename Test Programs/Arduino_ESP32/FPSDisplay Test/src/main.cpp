a40b9605a0bc50cacc981a1e83536af50b491a06
Tim Mooney <timmooney052@gmail.com>
2020-04-12 18:05:02 +1000

    added line for testing ssd1306 displays

--- Test Programs/Arduino_ESP32/FPSDisplay Test/src/main.cpp
+++ Test Programs/Arduino_ESP32/FPSDisplay Test/src/main.cpp
@@ -177,7 +177,8 @@
 typedef u8g2_uint_t u8g_uint_t;
 
 #define U8G2_16BIT 
-U8G2_SSD1327_MIDAS_128X128_F_HW_I2C u8g2(U8G2_R3, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 22, /* data=*/ 21);
+//U8G2_SSD1327_MIDAS_128X128_F_HW_I2C u8g2(U8G2_R3, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 22, /* data=*/ 21);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 22, /* data=*/ 21);
 
 #define SECONDS 10
 uint8_t flip_color = 0;
