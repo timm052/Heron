3b57923e9315ad4ffbebc52b11f3761716595170
Chris <chris.deakin.94@gmail.com>
2020-04-14 15:54:10 +1000

    Investigating 2 regs operation

--- Test Programs/Arduino_ESP32/ShiftRegTest/src/main.cpp
+++ Test Programs/Arduino_ESP32/ShiftRegTest/src/main.cpp
@@ -5,6 +5,7 @@ int RCK = 5; //Register Clock
 int SOT = 23; //Serial Out
 
 
+
 void setup() {
   pinMode(SerialClock, OUTPUT); //Set all pins to output
   pinMode(RCK, OUTPUT);
@@ -13,12 +14,12 @@ void setup() {
 }
 
 void loop() {
-  shiftOut(SOT, SerialClock, MSBFIRST,0b1010101010101010);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b1111111100000000);
   digitalWrite(RCK,HIGH);
   //delayMicroseconds(2);
   digitalWrite(RCK,LOW);
   delay(1000);
-  shiftOut(SOT, SerialClock, MSBFIRST,0b0101010101010101);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b0000000011111111);
   digitalWrite(RCK,HIGH);
   //delayMicroseconds(2);
   digitalWrite(RCK,LOW);
