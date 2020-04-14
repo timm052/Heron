cfea1c80fd62f29f38cf7993e84664ba81a1bdc4
Chris <chris.deakin.94@gmail.com>
2020-04-14 14:59:02 +1000

    Revision for Test

--- Test Programs/ShiftRegTest/src/main.cpp
+++ Test Programs/ShiftRegTest/src/main.cpp
@@ -1,23 +1,26 @@
 #include <Arduino.h>
 
-int SCK = 18; //Serial Clock
+int SerialClock = 18; //Serial Clock
 int RCK = 5; //Register Clock
 int SOT = 23; //Serial Out
 
 
 void setup() {
-  pinMode(SCK, OUTPUT);
+  pinMode(SerialClock, OUTPUT); //Set all pins to output
   pinMode(RCK, OUTPUT);
   pinMode(SOT, OUTPUT);
+  digitalWrite(RCK,LOW);
 }
 
 void loop() {
-  shiftOut(SOT, SCK, MSBFIRST,10101010);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b10101010);
   digitalWrite(RCK,HIGH);
+  //delayMicroseconds(2);
   digitalWrite(RCK,LOW);
   delay(1000);
-  shiftOut(SOT, SCK, MSBFIRST,01010101);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b01010101);
   digitalWrite(RCK,HIGH);
+  //delayMicroseconds(2);
   digitalWrite(RCK,LOW);
   delay(1000);
 }
