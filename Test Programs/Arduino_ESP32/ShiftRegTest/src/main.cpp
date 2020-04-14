22e80728a4168fc4b65c47b25a8adf3a69fb652c
Tim Mooney <timmooney052@gmail.com>
2020-04-14 16:32:56 +1000

    Revert "revert"

--- Test Programs/Arduino_ESP32/ShiftRegTest/src/main.cpp
+++ Test Programs/Arduino_ESP32/ShiftRegTest/src/main.cpp
@@ -3,7 +3,7 @@
 int SerialClock = 18; //Serial Clock
 int RCK = 5; //Register Clock
 int SOT = 23; //Serial Out
-unsigned int val = 65535;
+unsigned int val = 0b1010101010101010;
 
 
 void setup() {
@@ -14,14 +14,14 @@ void setup() {
 }
 
 void loop() {
-  shiftOut(SOT, SerialClock, MSBFIRST,0b11111111);
-  shiftOut(SOT, SerialClock, MSBFIRST,0b00000000);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b1010101010101010);
   digitalWrite(RCK,HIGH);
+  //delayMicroseconds(2);
   digitalWrite(RCK,LOW);
   delay(1000);
-  shiftOut(SOT, SerialClock, MSBFIRST,0b00000000);
-  shiftOut(SOT, SerialClock, MSBFIRST,0b11111111);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b1010101010101010);
   digitalWrite(RCK,HIGH);
+  //delayMicroseconds(2);
   digitalWrite(RCK,LOW);
   delay(1000);
 }
