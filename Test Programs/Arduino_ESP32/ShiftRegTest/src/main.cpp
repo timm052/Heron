1e5db1fd8417cc2601c32dc351ad6cbc057e21ec
Chris <chris.deakin.94@gmail.com>
2020-04-14 15:55:58 +1000

    Unsigned Int

--- Test Programs/Arduino_ESP32/ShiftRegTest/src/main.cpp
+++ Test Programs/Arduino_ESP32/ShiftRegTest/src/main.cpp
@@ -3,7 +3,7 @@
 int SerialClock = 18; //Serial Clock
 int RCK = 5; //Register Clock
 int SOT = 23; //Serial Out
-
+unsigned int val = 0b1111111100000000;
 
 
 void setup() {
@@ -14,7 +14,7 @@ void setup() {
 }
 
 void loop() {
-  shiftOut(SOT, SerialClock, MSBFIRST,0b1111111100000000);
+  shiftOut(SOT, SerialClock, MSBFIRST,val);
   digitalWrite(RCK,HIGH);
   //delayMicroseconds(2);
   digitalWrite(RCK,LOW);
