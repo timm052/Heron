150f5d42e824868f15c19a449bb90f9f4ffce3bf
Chris <chris.deakin.94@gmail.com>
2020-05-16 17:38:07 +1000

    Update main.cpp
    Trying another method for shift regs - both included and commented out.

--- Test Programs/Arduino_ESP32/ShiftRegTest/src/main.cpp
+++ Test Programs/Arduino_ESP32/ShiftRegTest/src/main.cpp
@@ -3,7 +3,8 @@
 int SerialClock = 18; //Serial Clock
 int RCK = 5; //Register Clock
 int SOT = 23; //Serial Out
-unsigned int val = 65535;
+unsigned int val1 = 0b1010101010101010;
+unsigned int val2 = 0b0101010101010101;
 
 
 void setup() {
@@ -14,14 +15,51 @@ void setup() {
 }
 
 void loop() {
-  shiftOut(SOT, SerialClock, MSBFIRST,0b11111111);
-  shiftOut(SOT, SerialClock, MSBFIRST,0b00000000);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b10101010);
+   digitalWrite(RCK,HIGH);
+  digitalWrite(RCK,LOW);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b01010101);
   digitalWrite(RCK,HIGH);
   digitalWrite(RCK,LOW);
   delay(1000);
-  shiftOut(SOT, SerialClock, MSBFIRST,0b00000000);
-  shiftOut(SOT, SerialClock, MSBFIRST,0b11111111);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b01010101);
+   digitalWrite(RCK,HIGH);
+  digitalWrite(RCK,LOW);
+  shiftOut(SOT, SerialClock, MSBFIRST,0b10101010);
   digitalWrite(RCK,HIGH);
   digitalWrite(RCK,LOW);
   delay(1000);
 }
+
+/*
+#include <Arduino.h>
+#include <SPI.h>
+
+int SerialClock = 18; //Serial Clock
+int RCK = 5; //Register Clock
+int SOT = 23; //Serial Out
+unsigned int val1 = 0b1010101010101010;
+unsigned int val2 = 0b0101010101010101;
+
+
+void setup() {
+
+  SPI.begin(SerialClock,20,SOT);
+}
+
+
+void loop()
+{
+  while(1)
+  {
+    digitalWrite(RCK,LOW);
+    SPI.transfer(0b01010101);
+    SPI.transfer(0b10101010);     
+    digitalWrite(RCK,HIGH);
+
+  
+  delay(300);
+  }
+
+  return;
+}*/
