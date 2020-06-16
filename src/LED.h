dd6fa01a8b2b5b65fc3e0805691ead1913b36a0b
timm052 <timmooney052@gmail.com>
2020-06-16 23:31:00 +1000

    implemented basic led functions
    Signed-off-by: timm052 <timmooney052@gmail.com>

--- src/LED.h
+++ src/LED.h
@@ -1,15 +1,34 @@
 // USEFUL LED FUNCTIONS
-
-
-void momentaryLED(int address, int time,  int colour){
+void momentaryLED(int address, int time,  int hue){
+    leds[address].setHue(hue);
+    delay(time)
+    leds[address] = CRGB::Black;
     return;
 }
 
-void Cursor(int range_stat, int range_stop, int bpm, int colour){
-    return;
-}
+// TODO: Cursor fuctionality needs basic step sequencer first
+//void Cursor(int play, int range_start, int bpm, int colour, CRGB ledbuffer){
+//    while(play);
+//    {
+//        for (size_t i = 0; i < 16; i++)
+//        {
+//            ledbuffer[i].setRGB(255, 255, 255);
+//            delay(bpm); //delay for one note of whatever zoom level device is at
+//            ledbuffer[i].setRGB(0, 0, 0);
+//        }  
+//    return;
+//}
 
-void toggleLED(int address, int state, int colour){
+void toggleLED(int address, bool state, int hue){
+    if (state == true)
+    {
+        leds[address].setHue(hue);
+    }
+    else
+    {
+        leds[address] = CRGB::Black;
+    }
+    
     return;
 }
 
