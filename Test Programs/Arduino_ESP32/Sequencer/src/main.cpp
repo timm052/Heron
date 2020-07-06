ba0dcbfe5400621ae81167547629eac870052d86
Chris <chris.deakin.94@gmail.com>
2020-07-06 15:17:14 +1000

    Sequencer again
    Updated to a form closer to what we'll actually use. LEDs now turn on and stay on when their button is pressed. Timer interrupt updates steps - doesn't do anything atm, just counts through steps.

--- Test Programs/Arduino_ESP32/Sequencer/src/main.cpp
+++ Test Programs/Arduino_ESP32/Sequencer/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 hw_timer_t * timer = NULL;
-int stepSize = 2; // Current max steps of sequencer - will be changed by user
+int stepSize = 2; // Current max steps of sequencer - will be changed by user. NOTE: size is this +1 (includes 0)
 int step = 0; // Current step - will update automatically
 #define TRACKS 2 // Max number of tracks - NOT USED ATM
 #define MAXSTEPS 3 // Max number of steps
@@ -16,7 +16,7 @@ int step = 0; // Current step - will update automatically
 
 
 
-
+// Each step of a track is one of these objects
 class stepNote
 {
   public:
@@ -29,27 +29,63 @@ class stepNote
 
 // Each track is an array of 'stepnote' objects - one object per step.
 volatile stepNote Track1[MAXSTEPS]; // Not sure where these should go, just global variables atm.
-//volatile stepNote Track2[MAXSTEPS];
+volatile stepNote Track2[MAXSTEPS];
 
+// Array of connected LEDs
 int LED[3] = {LED_PIN1, LED_PIN2, LED_PIN3}; 
 
+// Interrupt for timer - is called when timer reaches set interval
 void IRAM_ATTR onTimer()
 {
   if(step > stepSize) //If step has reached the end of sequence
   {step = 0;} //Reset step back to start
-    
-  if (step == 0) // If this is the first step
-  {digitalWrite(LED[2], LOW);} // Set the LED of final step off
-  else
-  {digitalWrite(LED[step-1], LOW);} // Set LED of previous step off
-  if(Track1[step].onOff) //If the step for this track is ON
-  {
-    digitalWrite(LED[step], HIGH);
-    //Then do whatever we do with contents of that stepNote
-  }
+
+  // Perform step activities here
+
+
+
   step++; //Increment the step
-  
-  
+}
+
+// Interrupts for button presses
+void IRAM_ATTR intB1()
+{    
+  if(Track1[0].onOff) // If step is on
+    {
+      Track1[0].onOff = 0; // Then turn step off
+      digitalWrite(LED[0], LOW); // And turn off LED
+    } 
+  else
+    {
+      Track1[0].onOff = 1; // Else turn step on
+      digitalWrite(LED[0], HIGH); // And turn on LED
+    }
+}
+void IRAM_ATTR intB2() // As above for button 2
+{
+  if(Track1[1].onOff) // If step is on
+    {
+      Track1[1].onOff = 0; // Then turn step off
+      digitalWrite(LED[1], LOW); // And turn off LED
+    } 
+  else
+    {
+      Track1[1].onOff = 1; // Else turn step on
+      digitalWrite(LED[1], HIGH); // And turn on LED
+    }
+}
+void IRAM_ATTR intB3() // As above for button 3
+{    
+   if(Track1[2].onOff) // If step is on
+    {
+      Track1[2].onOff = 0; // Then turn step off
+      digitalWrite(LED[2], LOW); // And turn off LED
+    } 
+  else
+    {
+      Track1[2].onOff = 1; // Else turn step on
+      digitalWrite(LED[2], HIGH); // And turn on LED
+    }
 }
 
 void setup() 
@@ -58,6 +94,10 @@ void setup()
   timerAttachInterrupt(timer, &onTimer, true); // Attaches 'onTimer' vector to timer interrupt. 'True' is edge, 'False' is level.
   timerAlarmWrite(timer, 500000, true); // 500k value, given prescaler above, will fire every 0.5s. True reloads value after done.
   timerAlarmEnable(timer);
+
+  attachInterrupt(BUT_PIN1, intB1, RISING); // Will call 'intB1' when button 1 pin rises from low to high
+  attachInterrupt(BUT_PIN1, intB2, RISING); // As above for button 2
+  attachInterrupt(BUT_PIN1, intB3, RISING); // As above for button 3
 
   //Setup LED and button pins
   pinMode(LED_PIN1, OUTPUT);
@@ -68,31 +108,8 @@ void setup()
   pinMode(BUT_PIN3, INPUT);
 }
 
-void loop() 
-{
-
-  if (digitalRead(BUT_PIN1)) // If button is pressed
-  {
-    if(Track1[0].onOff) // If step is on
-    {Track1[0].onOff = 0;} // Then turn step off
-    else
-    {Track1[0].onOff = 1;} // Else turn step on
-  }
-  if (digitalRead(BUT_PIN2)) //As above for button 2
-  {
-    if(Track1[1].onOff)
-    {Track1[1].onOff = 0;}
-    else
-    {Track1[1].onOff = 1;}
-  }
-  if (digitalRead(BUT_PIN3)) //As above for button 3
-  {
-    if(Track1[2].onOff)
-    {Track1[2].onOff = 0;}
-    else
-    {Track1[2].onOff = 1;}
-  }
-}
+void loop()
+{}
 
 
 
